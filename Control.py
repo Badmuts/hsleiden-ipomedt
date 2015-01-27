be9f7adf67fccbc229c8e09add98251216abbb4d
Daan Rosbergen <d.rosbergen@gmail.com>
2015-01-27 16:04:55 +0100

    Add stop function and button_pressed check

--- Control.py
+++ Control.py
@@ -14,16 +14,19 @@ class Control:
 		self.coil_A_2_pin = 17
 		self.coil_B_1_pin = 23
 		self.coil_B_2_pin = 24
+		self.buttonPin = 7
+		self.prev_state = 1
 
 		GPIO.setup(self.enable_pin, GPIO.OUT)
 		GPIO.setup(self.coil_A_1_pin, GPIO.OUT)
 		GPIO.setup(self.coil_A_2_pin, GPIO.OUT)
 		GPIO.setup(self.coil_B_1_pin, GPIO.OUT)
 		GPIO.setup(self.coil_B_2_pin, GPIO.OUT)
+		GPIO.setup(self.buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 		GPIO.output(self.enable_pin, 1)
 
-		self.delay = int(25) / 1000.0
+		self.delay = int(25) / 10000.0
 
 	# Home
 	# Down
@@ -31,14 +34,18 @@ class Control:
 		if status.active == False and status.y >= 0:
 			status.active = True
 			for i in range(0, steps):
-				self.setStep(1, 0, 0, 0)
-				time.sleep(self.delay)
-				self.setStep(0, 1, 0, 0)
-				time.sleep(self.delay)
-				self.setStep(0, 0, 1, 0)
-				time.sleep(self.delay)
-				self.setStep(0, 0, 0, 1)
-				time.sleep(self.delay)
+				if self.button_pressed() == False:
+					self.setStep(1, 0, 0, 0)
+					time.sleep(self.delay)
+					self.setStep(0, 1, 0, 0)
+					time.sleep(self.delay)
+					self.setStep(0, 0, 1, 0)
+					time.sleep(self.delay)
+					self.setStep(0, 0, 0, 1)
+					time.sleep(self.delay)
+				else:
+					break
+			# self.prev_state = 1
 			status.y = status.y + steps
 			GPIO.cleanup()
 			status.active = False
@@ -50,15 +57,19 @@ class Control:
 		if status.active == False and status.y > 0:
 			status.active = True
 			for i in range(0, steps):
-				self.setStep(0, 0, 0, 1)
-				time.sleep(self.delay)
-				self.setStep(0, 0, 1, 0)
-				time.sleep(self.delay)
-				self.setStep(0, 1, 0, 0)
-				time.sleep(self.delay)
-				self.setStep(1, 0, 0, 0)
-				time.sleep(self.delay)
-			status.y = status.y + steps
+				if self.button_pressed() == False:
+					self.setStep(0, 0, 0, 1) 
+					time.sleep(self.delay)
+					self.setStep(0, 0, 1, 0)
+					time.sleep(self.delay)
+					self.setStep(0, 1, 0, 0)
+					time.sleep(self.delay)
+					self.setStep(1, 0, 0, 0)
+					time.sleep(self.delay)
+				else:
+					break
+			# self.prev_state = 1
+			status.y = status.y - steps
 			GPIO.cleanup()
 			status.active = False
 			return
@@ -69,6 +80,17 @@ class Control:
 		GPIO.output(self.coil_A_2_pin, w2)
 		GPIO.output(self.coil_B_1_pin, w3)
 		GPIO.output(self.coil_B_2_pin, w4)
+
+	# stop
+	def button_pressed(self):
+		if status.active == True:
+			curr_state = GPIO.input(self.buttonPin)
+			if curr_state != self.prev_state and curr_state == 0:
+				self.prev_state = curr_state
+				return True
+			else:
+				# self.prev_state = curr_state
+				return False
 	# XPos
 	# YPos
 	# AutoOn
